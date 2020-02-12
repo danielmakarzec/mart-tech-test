bf9863d3a627e17405ca472fe45c336f24442914
daniel makarzec <danielmakarzec@gmail.com>
2020-02-12 12:46:19 +0000

    fix wrong class name bug in the show method test

--- spec/reader_unique_spec.rb
+++ spec/reader_unique_spec.rb
@@ -1,20 +1,27 @@
 require_relative '../lib/reader_unique'
 
 # create a ReaderUnique class.
-RSpec.describe Reader do
-  describe '#initialize' do
+RSpec.describe ReaderUnique do
+  describe 'ReaderUnique class' do
     context 'when creating a ReaderUnique class' do
       it 'exists' do
         expect(Reader).to be_truthy
       end
-    end
 
-  # check the class inherete from The Reader.
-  describe 'ReaderUnique class' do
-    it 'should inherit behavior from the Reader class' do
-      expect(ReaderUnique).to be < Reader
+      # check the class inherete from The Reader.
+      it 'should inherit behavior from the Reader class' do
+        expect(ReaderUnique).to be < Reader
+      end
+    end
+  end
+
+  # should return number of unique views of the given webpage.
+
+  describe '#show' do
+    context 'when procesing given file' do
+      it 'returns a number of views for each page' do
+        expect(ReaderUnique.new('./data/webserver.log').show[1].last).to eq(23)
+      end
     end
   end
 end
-
-  # should return number of unique views of the given webpage.
