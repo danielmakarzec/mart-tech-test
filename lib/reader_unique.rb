1119ae459d96104e65ae85568df50b0524da8a28
daniel makarzec <danielmakarzec@gmail.com>
2020-02-12 12:39:56 +0000

    edit list method to to return unique views numbers

--- lib/reader_unique.rb
+++ lib/reader_unique.rb
@@ -1,3 +1,15 @@
 class ReaderUnique < Reader
+  def initialize(file_path)
+    super
+  end
 
+  private
+
+  #  edit list method to get "unique" list
+
+  def list
+    super
+    file = File.open(@file_path)
+    file.uniq.map { |row| row.split(' ') }
+  end
 end
