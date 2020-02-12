8182c7bbf7483c0e9edd972d31621e3ab7266e7c
daniel makarzec <danielmakarzec@gmail.com>
2020-02-12 10:37:49 +0000

    prepare a list of tests I think is going to be required to complete the project

---
+++ spec/reader_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/reader'
+
+# create a Reader class.
+  # it schould initialize file path - string.
+  # check if the file exists.
+  # parse the file and return an array of views.
+  # should return number of views of the given webpage.
+
+# create a ReaderUnique class.
+  # check the class inherete from The Reader.
+  # should return number of unique views of the given webpage.
