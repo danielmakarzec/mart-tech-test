f29240aed07d143e2d67a3b8928a4e534e67f76a
daniel makarzec <danielmakarzec@gmail.com>
2020-02-12 10:45:25 +0000

    create first test. "See if Reader class exist"

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,6 +1,14 @@
 require_relative '../lib/reader'
 
 # create a Reader class.
+RSpec.describe Reader do
+  describe '#initialize' do
+    context 'when creating a Reader class' do
+      it 'exists' do
+        expect(Reader).to be_truthy
+      end
+    end
+  end
   # it schould initialize file path - string.
   # check if the file exists.
   # parse the file and return an array of views.
@@ -9,3 +17,4 @@ require_relative '../lib/reader'
 # create a ReaderUnique class.
   # check the class inherete from The Reader.
   # should return number of unique views of the given webpage.
+end
